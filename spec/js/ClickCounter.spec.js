f9975bbcc64dde9014d3f03c85b31c8f6e1d9364
Jason <JSilvis030592@yahoo.com>
2020-03-05 12:02:34 -0500

    Added companion method that increases companionNumber and decreases clickCount

--- spec/js/ClickCounter.spec.js
+++ spec/js/ClickCounter.spec.js
@@ -1,28 +1,70 @@
-describe("Click Count Calamity", function(){
+describe("Click Count Calamity", function() {
+
+    describe("Clicker", function() {
+
+
         let sut;
 
-        beforeEach(function(){
+        beforeEach(function() {
             sut = new ClickCounter();
-            sut.ClickCount = 0;
+            sut.clickCount = 0;
+            
         })
 
+        describe('ClickAction', function() {
 
-    describe('ClickAction', function(){
-
-        it('should increase ClickCounter by 1', function(){
-            //action
-            sut.clickAction()
-            expect(sut.clickCount).toBe(1);
+            it('should increase ClickCounter by 1', function() {
+                //action
+                sut.clickAction()
+                expect(sut.clickCount).toBe(1);
+            })
         })
+
+        describe('getClickCount', function() {
+
+            it('should return a ClickCount of 1 after clickAction is performed', function() {
+                //action
+                sut.clickAction();
+                expect(sut.getClickCount()).toBe(1);
+            })
+
+        })
+
+        describe('Click Companion', function () {
+            
+            describe('companionNumber property should be populated', function() {
+                it('should show companionNumber not to be null', function() {
+                    expect(sut.companionNumber).not.toBe(null);
+                })
+            })
+
+            describe('addClickCompanion should increase companionNumber', function(){
+                it('should show 1 for companionNumber', function(){
+                    sut.addCompanion();
+                    expect(sut.companionNumber).toBe(1);
+                })
+            })
+
+            describe('buyCompanion', function(){
+                it('should reduce clickCount by 100', function(){
+                    sut.clickCount = 100;
+                    sut.buyCompanion();
+                    expect(sut.clickCount).toBe(0);
+                })
+            })
+
+            describe('companionCost', function(){
+                it('should increase the cost of companion', function(){
+                    sut.clickCount = 100;
+                    sut.buyCompanion();
+                    expect(sut.companionCost).toBe(110)
+                })
+            })
+
+
+        })
+
     })
 
-    describe('getClickCount', function(){
 
-        it('should return a ClickCount of 1 after clickAction is performed', function(){
-            //action
-            sut.clickAction();
-            expect(sut.getClickCount()).toBe(1);
-        })
-        
-    })
 })
--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ class ClickCounter{
 
     constructor(){
         this.clickCount = 0;
+        this.companionNumber = 0;
+        this.companionCost = 100;
     }
 
     clickCount = 0;
@@ -13,4 +15,28 @@ class ClickCounter{
     getClickCount = function(){
         return this.clickCount;
     }
+
+    companionNumber = 0;
+    companionCost;
+
+    addCompanion = function(){
+        this.companionNumber++;
+    }
+
+    getCompanionNumber = function(){
+        return this.companionNumber;
+    }
+
+    buyCompanion = function(){
+        this.addCompanion();
+        this.clickCount = this.clickCount - this.companionCost;
+        this.companionCostIncrease()
+    }
+
+    companionCostIncrease = function () {
+        this.companionCost = this.companionCost + (this.companionCost * 0.1)    
+    }
+
+
+
 }
