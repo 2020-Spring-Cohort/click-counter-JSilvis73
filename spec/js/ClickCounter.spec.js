669b3520e8f1b8dd48df776188755aa4eb9dc9d9
Jason <JSilvis030592@yahoo.com>
2020-03-06 11:14:59 -0500

    Added compounder methods such as addCompounder, getCompunderNumber and buyCompounder

--- spec/js/ClickCounter.spec.js
+++ spec/js/ClickCounter.spec.js
@@ -8,7 +8,7 @@ describe("Click Count Calamity", function() {
         beforeEach(function() {
             sut = new ClickCounter();
             sut.clickCount = 0;
-            
+
         })
 
         describe('ClickAction', function() {
@@ -30,39 +30,39 @@ describe("Click Count Calamity", function() {
 
         })
 
-        describe('Click Companion', function () {
-            
+        describe('Click Companion', function() {
+
             describe('companionNumber property should be populated', function() {
                 it('should show companionNumber not to be null', function() {
                     expect(sut.companionNumber).not.toBe(null);
                 })
             })
 
-            describe('addClickCompanion should increase companionNumber', function(){
-                it('should show 1 for companionNumber', function(){
+            describe('addClickCompanion should increase companionNumber', function() {
+                it('should show 1 for companionNumber', function() {
                     sut.addCompanion();
                     expect(sut.companionNumber).toBe(1);
                 })
             })
 
-            describe('buyCompanion', function(){
-                it('should reduce clickCount by 100', function(){
+            describe('buyCompanion', function() {
+                it('should reduce clickCount by 100', function() {
                     sut.clickCount = 100;
                     sut.buyCompanion();
                     expect(sut.clickCount).toBe(0);
                 })
             })
 
-            describe('companionCost', function(){
-                it('should increase the cost of companion', function(){
+            describe('companionCost', function() {
+                it('should increase the cost of companion', function() {
                     sut.clickCount = 100;
                     sut.buyCompanion();
                     expect(sut.companionCost).toBe(110)
                 })
             })
 
-            describe('buyCompanion', function(){
-                it('buyCompanion should increase in cost after buyCompanion was clicked', function(){
+            describe('buyCompanion cost increase', function() {
+                it('buyCompanion should increase in cost after buyCompanion was clicked', function() {
                     sut.clickCount = 90;
                     sut.buyCompanion()
                     expect(sut.companionNumber).toBe(0)
@@ -73,7 +73,33 @@ describe("Click Count Calamity", function() {
 
         })
 
+        describe('Click Compounder', function() {
+
+            describe('compounderNumber property should be populated', function() {
+                it('should show compounderNumber not to be null', function() {
+                    expect(sut.compounderNumber).not.toBe(null);
+                })
+            })
+
+            describe('addCompounder should increase compounderNumber', function() {
+                it('should show 1 for compounderNumber', function() {
+                    sut.addCompounder();
+                    expect(sut.compounderNumber).toBe(1);
+                })
+            })
+
+            describe('buyCompounder should decrease clickCount', function() {
+                it('should reduce ClickCount by 10', function() {
+                    sut.clickCount = 10;
+                    sut.buyCompounder();
+                    expect(sut.clickCount).toBe(0);
+                })
+            })
+
+        })
+
+
+
+
     })
-
-
 })
--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,67 @@
-class ClickCounter{
+class ClickCounter {
 
-    constructor(){
+    constructor() {
         this.clickCount = 0;
         this.companionNumber = 0;
         this.companionCost = 100;
+        this.compounderNumber = 0;
+        this.compounderCost = 10;
     }
 
     clickCount = 0;
 
     clickAction = function() {
         this.clickCount++;
-        
+
     }
-    getClickCount = function(){
+    getClickCount = function() {
         return this.clickCount;
     }
 
     companionNumber = 0;
     companionCost;
 
-    addCompanion = function(){
+    addCompanion = function() {
         this.companionNumber++;
     }
 
-    getCompanionNumber = function(){
+    getCompanionNumber = function() {
         return this.companionNumber;
     }
 
-    buyCompanion = function(){
-        if(this.clickCount >= this.companionCost){
-        this.addCompanion();
-        this.clickCount = this.clickCount - this.companionCost;
-        this.companionCostIncrease()
+    buyCompanion = function() {
+        if (this.clickCount >= this.companionCost) {
+            this.addCompanion();
+            this.clickCount = this.clickCount - this.companionCost;
+            this.companionCostIncrease()
         }
     }
 
-    companionCostIncrease = function () {
-        this.companionCost = this.companionCost + (this.companionCost * 0.1)    
+    companionCostIncrease = function() {
+        this.companionCost = this.companionCost + (this.companionCost * 0.1)
     }
 
+    companionClickCount = function() {
+        this.clickCount = this.companionNumber + this.clickCount;
+    }
 
+    compounderCost;
+    compounderNumber = 0;
+
+    addCompounder = () => {
+        this.compounderNumber++;
+    }
+
+    getCompounderNumber = function() {
+        return this.compounderNumber;
+    }
+
+    buyCompounder = () => {
+        this.addCompounder();
+        this.clickCount = this.clickCount - this.compounderCost;
+    }
 
 }
+
+let mainCookieObject = new ClickCounter
+setInterval(mainCookieObject.companionClickCount(), 1000);
